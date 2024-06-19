0d24e00314bdc10e0be0f2884d4f4928e8bc50c2
Saaj Patel <saaj@uoguelph.ca>
2024-06-19 14:02:39 -0400

    Part 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,14 +2,14 @@ const Header = (prop) => {
 
   return (
 
-    <h1>{prop.course}</h1>
+    <h1>{prop.courses.name}</h1>
 
   )
 
 }
 
 const Part = (prop) => {
-  console.log(prop.prop)
+  
   return (
 
     <p>
@@ -46,27 +46,31 @@ const Total = (prop) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header courses = {course}/>
+      <Content parts = {course.parts}/> 
+      <Total parts = {course.parts}/>
     </div>
   )
 }
