4a7c56aa8bd37425d64ce37cfbef3421b05fe8db
Saaj Patel <saaj@uoguelph.ca>
2024-06-21 19:30:08 -0400

    unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,49 +1,39 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const Header = ({header}) => (<h1>{header}</h1>)
 
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+const Button = (props) => (
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
+
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const headers = {
+    feedback: 'give feedback',
+    statistics: 'statistics'
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <Header header = {headers.feedback}/>
+
+      <Button handleClick={() => (setGood(good + 1))} text={'good'}/>
+      <Button handleClick={() => (setNeutral(neutral + 1))} text={'neutral'}/>
+      <Button handleClick={() => (setBad(bad + 1))} text={'bad'}/>
+
+      <Header header = {headers.statistics}/>
+
+      <p>good {good}</p>
+      <p>meutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
