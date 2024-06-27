bb8aa50b91efc3760b9ffbccc596f3af6276581d
Saaj Patel <saaj@uoguelph.ca>
2024-06-27 19:10:59 -0400

    phonebook step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      phone: '040-1234567'
+     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setnewPhone] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      phone: newPhone
     }
 
     for (const person of persons) {
@@ -25,11 +29,16 @@ const App = () => {
     
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setnewPhone('')
 
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setnewPhone(event.target.value)
   }
 
   return (
@@ -42,13 +51,18 @@ const App = () => {
           onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+          value={newPhone}
+          onChange={handlePhoneChange} 
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-
       {persons.map(person =>
-        <div key={person.name}> {person.name} </div>
+        <div key={person.name}> {person.name} {person.phone} </div>
       )}
     </div>
   )
