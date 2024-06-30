a23455cda450179096544817f46ab2751a53cce2
Saaj Patel <saaj@uoguelph.ca>
2024-06-29 20:44:26 -0400

    Effect hooks

--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -2,6 +2,16 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import axios from 'axios'
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  console.log(notes)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
 const notes = [
   {
     id: 1,
