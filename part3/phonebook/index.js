ebe7ad9c184f98f669918f7d9433b0d3cc79420d
Saaj Patel <saaj@uoguelph.ca>
2024-07-14 01:16:08 -0400

    phonebook backend step 5, added http post request functionality, using Math.random to generate random id values

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,6 +60,26 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = {
+        id: `${Math.floor(Math.random() * 500)}`,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
