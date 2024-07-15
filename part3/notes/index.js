4f2d207d0373315b0439f084a87fa63f7a3b847b
Saaj Patel <saaj@uoguelph.ca>
2024-07-15 14:44:44 -0400

    notes

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
 
 let notes = [
   {
@@ -20,6 +19,21 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -73,6 +87,8 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
