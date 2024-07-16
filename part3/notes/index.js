7b3689c2afe4635029e9168e7a251cb3c6e8c648
Saaj Patel <saaj@uoguelph.ca>
2024-07-16 19:34:54 -0400

    Notes production version, with frontend and backend in one

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 
 let notes = [
   {
@@ -30,6 +32,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response, next) => {
   response.status(404).send({ error: 'unknown endpoint'})
@@ -90,7 +93,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
