f08bc45e29f40dcec5b8e748a06ea51f9afb5e7b
Saaj Patel <saaj@uoguelph.ca>
2024-07-16 20:09:55 -0400

    Added npm scripts in backend to streamline production build of the frontend

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,27 +1,13 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
-import noteService from './services/notes'
 import Notification from './components/Notification'
-
-const Footer = () => {
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic',
-    fontSize: 16
-  }
-  return (
-    <div style={footerStyle}>
-      <br/>
-      <em>Note app, Department of Computer Science, University of Helenski 2024</em>
-    </div>
-  )
-}
+import Footer from './components/Footer'
+import noteService from './services/notes'
 
 const App = () => {
-  const [notes, setNotes] = useState(null)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false) 
+  const [showAll, setShowAll] = useState(true) 
   const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
@@ -32,10 +18,6 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
-
-  if (!notes) { 
-    return null 
-  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -54,15 +36,14 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
-    const changedNote = {...note, important: !note.important}
+    const changedNote = { ...note, important: !note.important}
 
     noteService
       .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => notes.id !== id ? note : returnedNote))
-      })
+        .then(returnedNote => {
+          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        })
       .catch(error => {
         setErrorMessage(
           `Note '${note.content}' was already removed from the server`
@@ -70,7 +51,6 @@ const App = () => {
         setTimeout(() => {
           setErrorMessage(null)
         }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -83,33 +63,34 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage}/>
+    return (
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
+        <h1>Notes</h1>
+        <Notification message={errorMessage} />
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all' }
+          </button>
+        </div>      
+        <ul>
+          {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+        <input
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form>
+        <Footer />
       </div>
-      <ul>
-        {notesToShow.map(note => 
-          <Note 
-            key={note.id} 
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)}/>
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-        value={newNote} 
-        onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
-      <Footer/>
-    </div>
-  )
-}
+    )
+  }
 
 export default App 
