332b12d89145a3f5bf7f2a14f75eb276617fc5db
Saaj Patel <saaj@uoguelph.ca>
2024-07-17 01:07:34 -0400

    Added proxy for running app with json-server in development mode

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-const baseUrl = '/api/notes' //const baseUrl = 'http://localhost:3001/notes' 
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
+  console.log(request)
   return request.then(response => response.data)
 }
 
