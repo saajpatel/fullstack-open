ece49eeaafc96280ce0ef081f1a392441d797775
Saaj Patel <saaj@uoguelph.ca>
2024-07-30 13:59:38 -0400

    phonebook database step 3, deleting phonebook entries is also reflected in the database as well

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -78,9 +78,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+    Entry.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+
 })
 
 app.post('/api/persons', (request, response) => {
